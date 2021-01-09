9cbd35511d856927cc14e7a68e740a43818a30f7
CJ Dodge <hitmancjack@yahoo.com>
2021-01-09 14:31:46 -0800

    add Navbar with tabs and button for search

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Home />
     </div>
   );
