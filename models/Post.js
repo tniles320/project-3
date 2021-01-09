50802df446778d3a1b246466e53051aa40cdc167
Taylor Niles <tniles320@gmail.com>
2021-01-09 13:05:41 -0800

    adding schemas

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,43 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({});
+const PostSchema = new Schema({
+  userId: {
+    type: String,
+    trim: true,
+    required: "User ID is required",
+  },
+  title: {
+    type: String,
+    trim: true,
+    required: "Title is required",
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: "Description is required",
+  },
+  amount: {
+    type: String,
+    trim: true,
+    required: "Amount cannot be empty",
+  },
+  location: {
+    type: String,
+    trim: true,
+    required: "location is required",
+  },
+  messages: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Message",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
 const Post = mongoose.model("Post", PostSchema);
 
