a2c8e0396416c89dc24aca614a29f09502ca0d4a
Tom Mitchell <mitcht032@gmail.com>
2021-01-13 18:09:50 -0800

    switch account and dashboard styles

--- client/src/pages/Account/index.js
+++ client/src/pages/Account/index.js
@@ -1,13 +1,23 @@
 import React, { useContext } from "react";
 import UserContext from "../../utils/UserContext";
+import "./style.css";
 
 function Account() {
   const { username, email, zipCode, rating, posts } = useContext(UserContext);
 
   return (
     <div>
+      <div className="account-container">
+      <h2 className ="Header"> Welcome Back {username} {rating}</h2>
+      <br/>
+      <div className="account-info">
+      <h3>Account Info</h3>
       {username}
-      <h1>Hello</h1>
+        {email} {zipCode}
+        
+    </div>
+     
+    </div>
     </div>
   );
 }
--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import UserContext from "../../utils/UserContext";
-import "./style.css";
+
 import Navbar from "../../components/Navbar";
 
 function Dashboard(props) {
@@ -8,18 +8,10 @@ function Dashboard(props) {
   return (
     <div>
 <Navbar logout={props.logout} />
-      <div className="account-container">
-      <h2 className ="Header"> Welcome Back {username} {rating}</h2>
-      <br/>
-      <div className="account-info">
-      <h3>Account Info</h3>
-      {username}
-        {email} {zipCode}
-        
-    </div>
+<h1>Hello{username}</h1> 
     {posts}
     </div>
-    </div>
+    
   );
 }
 
