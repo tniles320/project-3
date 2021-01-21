e2f1636ae8f422d97a395f294d98bed495f89632
Taylor Niles <tniles320@gmail.com>
2021-01-20 20:37:00 -0800

    added function to filter posts

--- client/src/components/Sidebar/index.js
+++ client/src/components/Sidebar/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
 
-function Sidebar() {
+function Sidebar(props) {
+  const { handleFilter } = props;
   return (
     <div>
       {/* Find Work dropdowns */}
-      <form id ="find-work">
+      <form id="find-work">
         <fieldset>
           <legend>Find Work</legend>
           <p>
@@ -24,15 +25,18 @@ function Sidebar() {
               <option value="2">5+ Years</option>
             </select>
           </p>
+          <button id="find-work-filter" onClick={handleFilter}>
+            Filter
+          </button>
         </fieldset>
       </form>
       {/* Find a Worker dropdown */}
-      <form id ="find-worker">
+      <form id="find-worker">
         <fieldset>
           <legend>Find a Worker</legend>
           <p>
             <label>Work Type </label>
-            <select id="work-type">
+            <select id="worker-type">
               <option value="select">Select</option>
               <option value="Delivery & Moving">Delivery & Moving</option>
               <option value="Handyman">Handyman</option>
@@ -46,6 +50,9 @@ function Sidebar() {
               <option value="2">$$$</option>
             </select>
           </p>
+          <button id="find-worker-filter" onClick={handleFilter}>
+            Filter
+          </button>
         </fieldset>
       </form>
     </div>
--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -9,12 +9,15 @@ import "./style.css";
 
 function Dashboard(props) {
   const { handleLogout } = props;
+  const { _id } = useContext(UserContext);
 
   const [posts, setPosts] = useState([]);
   const handlePosts = () => {
     API.getPosts().then((res) => {
       res.data.map((post) => {
-        setPosts((posts) => [...posts, post]);
+        if (_id !== post.user) {
+          setPosts((posts) => [...posts, post]);
+        }
       });
     });
   };
@@ -23,11 +26,25 @@ function Dashboard(props) {
     handlePosts();
   }, []);
 
+  const handleFilter = (event) => {
+    event.preventDefault();
+    const wtype = document.getElementById("work-type");
+    const wrtype = document.getElementById("worker-type");
+    const worktype = wtype.options[wtype.selectedIndex].text;
+    const workertype = wrtype.options[wrtype.selectedIndex].text;
+    setPosts([]);
+    posts.map((post) => {
+      if (worktype === post.worktype) {
+        setPosts((posts) => [...posts, post]);
+      }
+    });
+  };
+
   return (
     <div>
       <Navbar handleLogout={handleLogout} />
       <div className="dash-container">
-        <Sidebar />
+        <Sidebar handleFilter={handleFilter} />
         <PostContainer posts={posts} />
       </div>
       <Footer />
