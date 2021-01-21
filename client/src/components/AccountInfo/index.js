5907b3db3d0ae342baa94f6daec9502f66938e7c
Tom Mitchell <mitcht032@gmail.com>
2021-01-20 18:20:19 -0800

    added stylying to account page and AccountInfo component. It's just a start.

--- client/src/components/AccountInfo/index.js
+++ client/src/components/AccountInfo/index.js
@@ -1,13 +1,13 @@
 import React, { useContext } from "react";
 import UserContext from "../../utils/UserContext";
-
+import "./style.css";
 
 // input forms for creating a post
 function AccountInfo(props) {
     const { username, email, zipCode, rating } = useContext(UserContext);
 
   return (
-    <div>
+    <div className="account-info-container">
     {username}
       <br/>
     {email}
